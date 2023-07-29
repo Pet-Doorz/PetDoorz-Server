059d8a97cf1bcbccc0eec7948dabea015d370004
DrunkenLee <michaelsinx.one@gmail.com>
2023-07-30 00:08:30 +0700

    feat: filterHotel , incomplete

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const HotelController = require("../controllers/HotelController");
 
 router.get("/", HotelController.findNearestHotel);
-router.get("/:id", HotelController.getDetail);
+router.put("/:id", HotelController.update);
 router.post("/login", HotelController.login);
 router.post("/register", HotelController.register);
 
