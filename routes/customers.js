27fe6f8c64875966770c916bd83810fe0583d881
raymcnd <89778453+raymcnd@users.noreply.github.com>
2023-07-30 20:19:50 +0700

    fix: customers login validation

--- routes/customers.js
+++ routes/customers.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const CustomerController = require("../controllers/CustomerController");
 
 router.get("/");
+router.post("/", CustomerController.register);
+router.post("/login", CustomerController.login);
 router.get("/:id", CustomerController.readCustomerById)
 router.put("/:id", CustomerController.editCustomer)
-router.post("/login", CustomerController.login);
-router.post("/", CustomerController.register);
 
 module.exports = router;
