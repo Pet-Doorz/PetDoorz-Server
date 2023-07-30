09e2997813bcc0c655e6264e4f80c60bbc016722
raymcnd <89778453+raymcnd@users.noreply.github.com>
2023-07-31 00:46:21 +0700

    feat: authorization related to booking

--- middlewares/authorizationBookingDone.js
+++ middlewares/authorizationBookingDone.js
@@ -1,2 +1,22 @@
 // Supaya customer hanya bisa mengubah status booking miliknya saja
 // Hanya bisa booking yang berstatus process
+
+const { Booking } = require("../models")
+
+async function authorizationBookingDone(req, res, next) {
+    try {
+        const { id: bookingId } = req.params;
+        const { id: customerId } = req.customer;
+
+        const booking = await Booking.findByPk(bookingId)
+        if (!booking) throw { name: "NOTFOUND" }
+        if (booking.status !== "process") throw { name: "InvalidBooking"}
+        if (booking.CustomerId !== customerId) throw { name: "Forbidden"}
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = authorizationBookingDone
--- middlewares/authorizationBookingProcess.js
+++ middlewares/authorizationBookingProcess.js
@@ -1,2 +1,28 @@
 // Supaya hotel hanya bisa update status booking miliknya
 // Hanya bisa booking yang berstatus booked
+
+const { Booking, Room, Hotel } = require("../models")
+
+async function authorizationBookingProcess(req, res, next) {
+    try {
+        const { id: bookingId } = req.params;
+        const { id: hotelId } = req.hotel;
+
+        const booking = await Booking.findByPk(bookingId, {
+            include: {
+                model: Room,
+                include: Hotel
+            }
+        })
+        console.log(booking.Room.Hotel.id)
+        if (!booking) throw { name: "NOTFOUND" }
+        if (booking.status !== "booked") throw { name: "InvalidBooking"}
+        if (booking.Room.Hotel.id !== hotelId) throw { name: "Forbidden"}
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = authorizationBookingProcess
