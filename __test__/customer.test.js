fa0e4bbbcde176deb05da8c2b360a26016b2fe91
Leonardo Ringo <leonardoringo12@gmail.com>
2023-07-31 21:43:06 +0700

    test: done test for endpoint /customers/login

--- __test__/customer.test.js
+++ __test__/customer.test.js
@@ -139,4 +139,109 @@ describe('POST /customers', () => {
         expect(response.status).toBe(400)
         expect(response.body).toHaveProperty('message', 'Phone number is required')
     })
+
+    test('Register fail, invalid email format', async () => {
+        const bodyReq = {
+            email: 'register',
+            password: 'register',
+            fullName: 'register',
+            phoneNumber: '080808'
+        }
+    
+        const response  = await request(app).post('/customers').send(bodyReq)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('message', 'Invalid email format')
+    })
+
+    test('Register fail, email already used', async () => {
+        const bodyReq = {
+            email: 'user1@mail.com',
+            password: 'register',
+            fullName: 'register',
+            phoneNumber: '080808'
+        }
+
+        await Customer.create(bodyReq)
+    
+        const response  = await request(app).post('/customers').send(bodyReq)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('message', 'Email must be unique')
+    })
+})
+
+describe('ENDPOINT /customers/login', () => {
+    test('Login success!', async () => {
+        const bodyReq = {
+            email: 'register@mail.com',
+            password: 'register'
+        }
+
+        const response = await request(app).post('/customers/login').send(bodyReq)
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('access_token', expect.any(String))
+    })
+
+    test('Login fail, no email', async () => {
+        const bodyReq = {
+            password: 'register'
+        }
+
+        const response = await request(app).post('/customers/login').send(bodyReq)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('message', 'Email is required')
+    })
+
+    test('Login fail, no password', async () => {
+        const bodyReq = {
+            email: 'register@mail.com'
+        }
+
+        const response = await request(app).post('/customers/login').send(bodyReq)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('message', 'Password is required')
+    })
+
+    test('Login fail, email is empty string', async () => {
+        const bodyReq = {
+            email: '',
+            password: 'register'
+        }
+
+        const response = await request(app).post('/customers/login').send(bodyReq)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('message', 'Email is required')
+    })
+
+    test('Login fail, password is empty string', async () => {
+        const bodyReq = {
+            email: 'register@mail.com',
+            password: ''
+        }
+
+        const response = await request(app).post('/customers/login').send(bodyReq)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('message', 'Password is required')
+    })
+
+    test('Login fail, invalid email', async () => {
+        const bodyReq = {
+            email: 'register1@mail.com',
+            password: 'register'
+        }
+
+        const response = await request(app).post('/customers/login').send(bodyReq)
+        expect(response.status).toBe(401)
+        expect(response.body).toHaveProperty('message', 'Invalid email/password')
+    })
+
+    test('Login fail, invalid password', async () => {
+        const bodyReq = {
+            email: 'register1@mail.com',
+            password: 'registertest'
+        }
+
+        const response = await request(app).post('/customers/login').send(bodyReq)
+        expect(response.status).toBe(401)
+        expect(response.body).toHaveProperty('message', 'Invalid email/password')
+    })
 })
